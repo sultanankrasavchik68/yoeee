9fbd4b3de8391ec85a22fcc6b669ad10bcd2ea4f
sultanankrasavchik68 <sultananvarov34@gmail.com>
2025-03-13 09:07:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let info = prompt('String typedagi malumot kriting')
+ console.log(info)
+
+
+ let age = prompt('Number typedagi malumot kriting')
+ console.log(age)
+
+ let arr = prompt('boolean typedagi malumot kriting')
+ console.log(arr)
+
+ alert(`String typedagi malumot ${info } \nNumber typedagi malumot' ${age} \nboolean typedagi malumot ${arr} `)
+
